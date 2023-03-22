9580241e5ccbd351b27ccb4c7d5b0b32dc5b0058
afang <fangfang071288@gmail.com>
2023-03-22 13:13:41 +0800

    classpeople 人數迴圈

--- my-app/src/class/Index.js
+++ my-app/src/class/Index.js
@@ -150,7 +150,7 @@ function Index() {
             <span className="col-auto title j-deepSec">購買須知</span>
             <div className="title-line d-block d-md-none"></div>
           </div>
-          <div className="purchase ps-xl-4">
+          <div className="purchase ps-xl-4 ">
             <ul>
               <li className="j-h3">
                 當參加人數未達上述規定的最少人數時，將取消課程，於上課前 2
@@ -163,7 +163,7 @@ function Index() {
               </li>
             </ul>
           </div>
-          <div className=" d-flex justify-content-center">
+          <div className=" d-flex justify-content-center pt-3">
             <Link className="o-long-btn j-h3 " to="/class/Classsec">
               報名課程
             </Link>
